f32b86c6252f186da822d034435736204bc68be0
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-10 07:56:12 +0200

    defined the desired regex pattern for a valid email address and extracted the email from the input field to output it in the other textarea

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let button = document.getElementById("button");
 let output = document.getElementById("output");
+let validEmail = /([a-zA-Z0-9-._]+@[a-zA-Z0-9-._]+\.+[a-zA-Z0-9-_.]+)/g;
 
 function getEmails() {
   let input = document.getElementById("input").value;
-  console.log(input);
+  output.innerHTML = input.match(validEmail);
+  console.log("Email found!");
 }
 button.addEventListener("click", getEmails);
